e9d8c281dd9b3ff24b11d410f32187c73f710512
Elzbieta <ella13162@gmail.com>
2023-09-04 11:31:55 +0100

    few changes

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
 const express = require('express');
-const path = require("path")
+const mongoose = require('mongoose');
+const path = require('path');
+const { Product } = require('./models/models');
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-// Set the views directory and the view engine
-app.set('views', path.join(__dirname, 'views'));
+// Database connection
+mongoose.connect('mongodb://localhost:27017/DIAMOND_SHOP', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Set view engine and static folder
 app.set('view engine', 'ejs');
-
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-    res.render('home'); // Render the home.ejs
+// Routes
+app.get('/', async (req, res) => {
+  const products = await Product.find();
+  res.render('products', { products });
 });
 
-app.get("/contact", (req, res) => {
-    res.render('contact'); // Render the contact.ejs
+app.get('/product/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id); // Retrieve the product by its ID
+    res.render('product', { product: product });
+  } catch (error) {
+    console.log(error);
+    res.status(500).render('error', { errorMessage: 'Something broke!' });
+  }
 });
 
-app.get("/clients", (req, res) => {
-    res.render('clients'); // Render the clients.ejs
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
-app.get("/login", (req, res) => {
-    res.render('login'); // Render the login.ejs
+app.get('/clients', (req, res) => {
+  res.render('clients');
 });
 
-app.get("/basket", (req, res) => {
-    res.render('basket'); // Render the basket.ejs
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
-app.get("/products", (req, res) => {
-    res.render('products'); // Render the products.ejs
+app.get('/basket', (req, res) => {
+  // Logic to retrieve basket items from database
+  const basketItems = []; // Replace with actual logic
+  res.render('basket', { basketItems });
 });
 
-app.get('/error', (req, res) => {
-    const errorMessage = 'Something went wrong!';
-    res.render('error', { errorMessage });
+// Error Routes
+app.use((req, res) => {
+  res.status(404).render('error', { errorMessage: 'Page not found' });
 });
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render('error', { errorMessage: 'Something broke!' });
+});
+
+// Start server
 app.listen(port, () => {
-    console.log(`Server start on port: ${port}`);
+  console.log(`Server started on port: ${port}`);
 });
---
+++ md notes/6_everything.md
@@ -0,0 +1,8 @@
+# Step 1:
+ update on connection to mongo db in index.js
+
+# Step 2:
+ update on basket.ejs and product.ejs
+
+# Step 3:
+ 
